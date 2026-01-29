808e3ee7a916135336dd8ffb908d5c9b38fb37bc
venna <vennaviene@icloud.com>
2026-01-29 17:27:46 +0300

    Initial Commit

---
+++ How Many Days from Your Born/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  How Many Days from Your Born
+//
+//  Created by Fırat Sarar on 29.01.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ How Many Days from Your Born/How_Many_Days_from_Your_BornApp.swift
@@ -0,0 +1,17 @@
+//
+//  How_Many_Days_from_Your_BornApp.swift
+//  How Many Days from Your Born
+//
+//  Created by Fırat Sarar on 29.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct How_Many_Days_from_Your_BornApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
